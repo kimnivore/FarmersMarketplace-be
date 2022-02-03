e05f0aa32ae386241fe1cd753b66a81217cef3e8
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-02-02 20:33:20 -0700

    created tests

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -22,3 +22,43 @@ describe('server.js', () => {
     expect(process.env.NODE_ENV).toBe('testing')
   })
 })
+
+describe('[POST] Auth Endpoints', () => {
+test('[1] Successfully registered new user', async () => {
+  const res = await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'John', password: '1234' })
+  expect(res.status).toBe(201)
+  expect(res.body.message).toMatch(/Successfully registered John!/i);
+
+})
+
+test('[2] Returns correct error message when username already exists', async () => {
+  const res = await request(server)
+  .post('/api/auth/register')
+  .send({ username: 'sam', password: '1234' })
+
+  expect(res.status).toBe(401)
+  expect(res.body.message).toMatch(/Username taken, please choose another one./i);
+
+})
+
+  test('[3] User can login successfully', async() => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'frodo', password: '5678' })
+ 
+    expect(res.status).toBe(200)
+    expect(res.body.message).toMatch(/welcome, frodo/i);
+  });
+  
+
+  test('[4] Returns correct error message when credentials are invalid', async ()=> {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'John',password: '1234' })
+    expect(res.status).toBe(401)
+    expect(res.body.message).toMatch(/Invalid credentials/i);
+  })
+
+})
